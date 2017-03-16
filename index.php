ea4d2e10ee6f6bf45e16fbc99492b611b4e179e5
Iks <yahm93@gmail.com>
2017-03-17 00:39:49 +0100

    First commit
    Creation des fichiers de base.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>I LOVE U</title>
+		<script type="text/javascript"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<form method = "post" action = "index.php">
+			<div>
+				<button type = "submit">Love</button>
+			</div>
+		</form>
+	</body>
+</html>
