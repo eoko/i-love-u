5546f81a2af3f7f16a470a4b6dcf12e1e8d46b4c
Iks <yahm93@gmail.com>
2017-03-17 03:09:45 +0100

    Second commit(suite3)

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
 		<title>I LOVE U</title>
 		<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
 		<script src ="js/traitement.js"></script>
-		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
 		<p>Your Like counts,please press the like button.</p>
 		<form method = "post" action = "index.php">
 			<button class = "button" type = "submit"/>
 		</form>
+		
+		<div>
+			<p>So far we got : </p>
+			<p>likes</p>
+		</div>
 
 	</body>
 </html>
--- js/traitement.js
+++ js/traitement.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
     $.getJSON('https://api.ipify.org?format=json', function(data) {
-    	alert(data.ip);
+    	//alert(data.ip);
 	});
 });
