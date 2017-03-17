677816312dbe9cb6190da12526331244729786b5
Iks <yahm93@gmail.com>
2017-03-17 02:46:52 +0100

    Second commit
    Arrangement de l'espace de travail.
    Début d'implementation de code.

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html>
 	<head>
 		<title>I LOVE U</title>
-		<script type="text/javascript"></script>
+		<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
+		<script src ="traitement.js"></script>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
+		<p>Your Like counts,please press the like button.</p>
 		<form method = "post" action = "index.php">
-			<div>
-				<button type = "submit">Love</button>
-			</div>
+			<button class = "button" type = "submit"/>
 		</form>
+
 	</body>
 </html>
---
+++ js/traitement.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    $.getJSON('https://api.ipify.org?format=json', function(data) {
+    	alert(data.ip);
+	});
+});
