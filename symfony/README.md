f0401035c350a58f78c961faeed01cd152eabd47
Axel Blazart <a.blazart@gmail.com>
2017-09-15 16:37:33 +0200

    projet

---
+++ symfony/README.md
@@ -0,0 +1,4 @@
+temp
+====
+
+A Symfony project created on September 15, 2017, 12:42 pm.
---
+++ symfony/src/LuvBundle/LuvBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LuvBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LuvBundle extends Bundle
+{
+}
